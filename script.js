48d1295e5dc3845dae4ef8a9e72b94fa8657387c
Hafizzargar <96607751+Hafizzargar@users.noreply.github.com>
2024-01-16 02:32:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 //your JS code here. If required.
 let a=prompt("enter 1st number");
 let b=prompt("enter 2nd number");  
-if(parseInt(a)==parseInt(b)){
+a=parseFloat(a);
+b=parseFloat(b);
+if(a===b){
 	alert("true");
 }else{
 	alert("false");
